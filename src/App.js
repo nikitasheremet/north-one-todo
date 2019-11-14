c95da54bd5ef04fdb83bac5168c79db88c50ae23
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-11-14 11:23:56 -0500

    set useState to empty objec as default, add function to add a new to do based on title, description, dueDate and status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.sass";
 
 function App() {
-  return <div className="App"></div>;
+  const [toDos, setToDos] = useState({});
+
+  const addToDo = () => {
+    let id = Object.values(toDos).length + 1;
+    setToDos({ ...toDos, [id]: { title, dueDate, status, description } });
+  };
+
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
